ee4ce27a57e3ef25480e1a39289d2b30a018fbfd
Dave Cooper <dave@davecooper.dev>
2021-12-06 15:41:35 +0000

    day 6

---
+++ day-06/part-1.ts
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+const input = fs
+  .readFileSync("day-06/input.txt", "utf8")
+  .split(",")
+  .map(Number);
+
+const init = [...Array(9)].map((_, i) => input.filter((x) => x === i).length);
+
+const step = (state) => {
+  const births = state.shift();
+
+  state.push(births);
+  state[6] += births;
+
+  return state;
+};
+
+const solve = (numDays) =>
+  [...(Array(numDays).keys() as any)]
+    .reduce(step, init)
+    .reduce((a, b) => a + b, 0);
+
+console.log(solve(80));
+
+export {};
